fc0103f6952a8e470a4d8b4cdc272868b7b6bf6c
kee cha <kee_cha18@yahoo.com>
2019-11-27 09:05:48 -0600

    added user input for how much items to use

--- Recipe.cs
+++ Recipe.cs
@@ -21,12 +21,35 @@ namespace LemonadeStand_3DayStarter
             pricePerCup = 0;
         }
 
-        public int MakeRecipe()
+        public int AmountOfLemonsUse()
         {
             Console.WriteLine("How many lemons do you want to use?");
             int lemonAmount = Int32.Parse(Console.ReadLine());
             amountOfLemons += lemonAmount;
             return amountOfLemons;
         }
+        public int AmountOfSugarUse()
+        {
+            Console.WriteLine("How many sugar cubes do you want to use?");
+            int sugarAmount = Int32.Parse(Console.ReadLine());
+            amountOfSugarCubes += sugarAmount;
+            return amountOfSugarCubes;
+        }
+        public int AmountOfIceUse()
+        {
+            Console.WriteLine("How many ice cubes do you want to use?");
+            int iceCubeAmount = Int32.Parse(Console.ReadLine());
+            amountOfIceCubes += iceCubeAmount;
+            return amountOfIceCubes;
+        }
+        public double CostPerCup()
+        {
+            Console.WriteLine("How much would you like to sell a cup of lemonade for?");
+            double price = Int32.Parse(Console.ReadLine());
+            pricePerCup = price;
+            return pricePerCup;
+        }
+
+
     }
 }
